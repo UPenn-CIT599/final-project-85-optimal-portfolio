22e4e4c4b2eafd0e20d97273bd7c704a85cb521c
Seseg <sesegb@seas.upenn.edu>
2019-11-14 11:21:56 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Runner {
     public static void main(String[] args) throws FileNotFoundException {
+        //will read either ROA.csv or leverageratio.csv
         RoaReader roaReader = new RoaReader("ROA.csv");
         roaReader.run();
     }
