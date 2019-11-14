16593f5ae593711afa986c83012ed143d529cead
Seseg <sesegb@seas.upenn.edu>
2019-11-14 11:50:25 -0500

    Update HelperMethods.java
    Added find key with minimum value method

--- HelperMethods.java
+++ HelperMethods.java
@@ -1,7 +1,15 @@
 import java.util.HashMap;
 import java.util.Set;
 
+/**
+ * General help methods to work with data structures
+ */
 public class HelperMethods {
+    /**
+     * For ROA computation
+     * @param aHashMap
+     * @return
+     */
     public String findKeyWithMaxValue(HashMap<String, Double> aHashMap) {
         Set<String> loopSet = aHashMap.keySet();
         double currentMaxValue = 0;
@@ -14,5 +22,24 @@ public class HelperMethods {
             }
         }
         return keyWithMaxValue;
+    }
+
+    /**
+     * For Leverage ratio computation
+     * @param aHashMap
+     * @return
+     */
+    public String findKeyWithMinValue(HashMap<String, Double> aHashMap) {
+        Set<String> loopSet = aHashMap.keySet();
+        double currentMinValue = 0;
+        String keyWithMinValue = "";
+        for (String key : loopSet) {
+            double currentValue = aHashMap.get(key);
+            if (currentMinValue > currentValue) {
+                currentMinValue = currentValue;
+                keyWithMinValue = key;
+            }
+        }
+        return keyWithMinValue;
     }
 }
