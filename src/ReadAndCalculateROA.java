40927857c9a8eb31cfbe44175df815bff0909018
SesegBm <57723645+SesegBm@users.noreply.github.com>
2019-12-01 08:20:22 -0500

    Create ReadAndCalculateROA.java

---
+++ src/ReadAndCalculateROA.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+/**
+ * this class reads and calculates ROA stocks
+ */
+public class ReadAndCalculateROA {
+    /**
+     * @throws FileNotFoundException
+     */
+    public void run() throws FileNotFoundException {
+        Reader topMaxReader = new Reader("ROA.csv");
+        HashMap<String, Double> roaMax = topMaxReader.readCSV("roa");
+        Writer myWriter = new Writer();
+
+        //open file for writing data
+        File out = new File("roa-results.txt");
+        PrintWriter pw = new PrintWriter(out);
+
+        //write header to file
+        pw.println("20 Highest ROA Stocks:");
+        myWriter.writeToFile(roaMax, pw, "max");
+
+        pw.flush();
+        pw.close();
+    }
+}
