481e73552108d8ebb32630d79ec53581f916a20c
SesegBm <57723645+SesegBm@users.noreply.github.com>
2019-12-01 08:20:45 -0500

    Update Reader.java

--- src/Reader.java
+++ src/Reader.java
@@ -1,8 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
 
+/**
+ * general reader class
+ */
 public class Reader {
     private String filename;
 
@@ -15,16 +19,27 @@ public class Reader {
         this.filename = filename;
     }
 
+    static class DescOrder implements Comparator<Double> {
+
+        @Override
+        public int compare(Double o1, Double o2) {
+            return o2.compareTo(o1);
+        }
+    }
+
     /**
      * function reads data from CSV file into data structure
      *
      * @return
      * @throws FileNotFoundException
      */
-    public HashMap readCSV() throws FileNotFoundException {
+    public HashMap readCSV(String flag) throws FileNotFoundException {
         File file = new File(filename);
         Scanner sc = new Scanner(file);
-        HashMap<String, Double> topTwenty = new HashMap<>();
+        HashMap<String, Double> averageForEachStock = new HashMap<>();
+        //leverageRatioSet with TreeMap
+        TreeMap<Double, String> leverageRatioSet = new TreeMap<>();
+        TreeMap<Double, String> ROASet = new TreeMap<>(new DescOrder());
         sc.nextLine(); //skip header row
         //read data
         while (sc.hasNextLine()) {
@@ -32,20 +47,68 @@ public class Reader {
             int counter = 0;
             String line = sc.nextLine();
             String[] piecesOfLine = line.split(",");
-            //skip last line (it's SPX index line)
-            if (piecesOfLine[0].equals("SPX Index")) {
-                continue;
-            }
             for (int i = 1; i < piecesOfLine.length; i++) {
                 if (!piecesOfLine[i].isEmpty()) {
                     counter++;
-//                    doubleTemp += Double.parseDouble(piecesOfLine[i].substring(0, piecesOfLine[i].length() - 1));
                     doubleTemp += Double.parseDouble(piecesOfLine[i]);
                 }
             }
-            topTwenty.put(piecesOfLine[0], doubleTemp / counter);
+            BigDecimal bd = new BigDecimal(Double.toString(doubleTemp/counter));
+            bd = bd.setScale(4, RoundingMode.HALF_UP);
+            double roundedResult = bd.doubleValue();
+            averageForEachStock.put(piecesOfLine[0], roundedResult);
+            leverageRatioSet.put(roundedResult, piecesOfLine[0]);
+            ROASet.put(roundedResult, piecesOfLine[0]);
+        }
+        //get "SPX Index" value
+        Double spxIndexAverage = averageForEachStock.get("SPX Index");
+        //initialize set to loop through by keys
+        Set<String> loopSet = averageForEachStock.keySet();
+        //will hold all keys that needs to be removed
+        ArrayList<String> keysToBeRemoved = new ArrayList<>();
+        //loop through HashMap
+        for (String key : loopSet) {
+            double currentValue = averageForEachStock.get(key);
+            if (flag.equals("roa")) {
+                if (spxIndexAverage > currentValue) {
+                    System.out.println("roa: " + spxIndexAverage + " > " + currentValue);
+                    //build array of strings with keys to be removed
+                    keysToBeRemoved.add(key);
+                }
+            } else if (flag.equals("leverage-ratio")) {
+                if (spxIndexAverage < currentValue) {
+                    System.out.println("leverage-ratio: " + spxIndexAverage + " < " + currentValue);
+                    //build array of strings with keys to be removed
+                    keysToBeRemoved.add(key);
+                }
+            }
+        }
+        //remove all flagged keys
+        for (int i = 0; i < keysToBeRemoved.size(); i++) {
+            System.out.println("KEYS ARE BEING REMOVED");
+            ROASet.remove(averageForEachStock.get(keysToBeRemoved.get(i)));
+            leverageRatioSet.remove(averageForEachStock.get(keysToBeRemoved.get(i)));
+            averageForEachStock.remove(keysToBeRemoved.get(i));
         }
         sc.close();
-        return topTwenty;
+        System.out.println("leverage: " + leverageRatioSet);
+        int ii = 1;
+        for (Map.Entry<Double, String> a : leverageRatioSet.entrySet()) {
+            System.out.println(ii + " KEY->" + a.getKey() + " VALUE->" + a.getValue());
+            if (ii == 20) {
+                break;
+            }
+            ii++;
+        }
+        System.out.println("ROA: " + ROASet);
+        int jj = 1;
+        for (Map.Entry<Double, String> a : ROASet.entrySet()) {
+            System.out.println(jj + " KEY->" + a.getKey() + " VALUE->" + a.getValue());
+            if (jj == 20) {
+                break;
+            }
+            jj++;
+        }
+        return averageForEachStock;
     }
 }
